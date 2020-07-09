c5e1bccd0b389f9dad44551b7d5499daf1db4349
is-watering <24697900+is-watering@users.noreply.github.com>
2020-07-10 02:25:53 +0900

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,14 @@
+# fzf
 set -U FZF_LEGACY_KEYBINDINGS 0
+
+# keychain
+if status --is-interactive
+    keychain --quiet --agents ssh
+end
+
+begin
+    set -l HOSTNAME (hostname)
+    if test -f ~/.keychain/$HOSTNAME-fish
+        source ~/.keychain/$HOSTNAME-fish
+    end
+end
